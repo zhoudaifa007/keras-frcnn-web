b6924f7582be482372df7f79736d9ffc65501320
zhoudaifa007 <zhoudaifa007@gmail.com>
2022-08-28 16:59:59 +0800

    替换

--- test_frcnn.py
+++ test_frcnn.py
@@ -107,7 +107,7 @@ if C.network == 'resnet50':
 elif C.network == 'vgg':
 	num_features = 512
 
-if K.image_dim_ordering() == 'th':
+if K.image_data_format() == 'th':
 	input_shape_img = (3, None, None)
 	input_shape_features = (num_features, None, None)
 else:
@@ -151,7 +151,7 @@ visualise = True
 def tf_fit_img(img):
 	X, ratio = format_img(img, C)
 
-	if K.image_dim_ordering() == 'tf':
+	if K.image_data_format() == 'tf':
 		X = np.transpose(X, (0, 2, 3, 1))
 
 	# get the feature maps and output from the RPN
@@ -159,7 +159,7 @@ def tf_fit_img(img):
 	[Y1, Y2, F] = model_rpn.predict(X)
 
 
-	R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering(), overlap_thresh=0.7)
+	R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_data_format(), overlap_thresh=0.7)
 
 	# convert from (x1,y1,x2,y2) to (x,y,w,h)
 	R[:, 2] -= R[:, 0]
@@ -253,7 +253,7 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 
 	X, ratio = format_img(img, C)
 
-	if K.image_dim_ordering() == 'tf':
+	if K.image_data_format() == 'tf':
 		X = np.transpose(X, (0, 2, 3, 1))
 
 	# get the feature maps and output from the RPN
@@ -261,7 +261,7 @@ for idx, img_name in enumerate(sorted(os.listdir(img_path))):
 	[Y1, Y2, F] = model_rpn.predict(X)
 
 
-	R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_dim_ordering(), overlap_thresh=0.7)
+	R = roi_helpers.rpn_to_roi(Y1, Y2, C, K.image_data_format(), overlap_thresh=0.7)
 
 	# convert from (x1,y1,x2,y2) to (x,y,w,h)
 	R[:, 2] -= R[:, 0]
